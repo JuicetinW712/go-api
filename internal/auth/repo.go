ee4a28feba263c6dcef31dd6a4202ae951515a57
Justin <jwong47@u.rochester.edu>
2026-01-21 01:01:05 -0500

    Implemented base auth repo

--- internal/auth/repo.go
+++ internal/auth/repo.go
@@ -2,9 +2,10 @@ package auth
 
 import (
 	"database/sql"
+	"fmt"
 )
 
-type AuthRepository struct{
+type AuthRepository struct {
 	db *sql.DB
 }
 
@@ -14,10 +15,41 @@ func NewAuthRepository(db *sql.DB) *AuthRepository {
 
 // TODO - implement
 func (ar *AuthRepository) CreateUser(user AuthUser) error {
+	query := `
+		INSERT INTO users (id, username, email, password_hash) 
+		VALUES ($1, $2, $3, $4)`
+
+	_, err := ar.db.Exec(query,
+		user.ID,
+		user.Username,
+		user.Email,
+		user.PasswordHash,
+	)
+
+	if err != nil {
+		return fmt.Errorf("inserting user: %w", err)
+	}
+
 	return nil
 }
 
-// TODO - implement
 func (ar *AuthRepository) GetUser(username string) (AuthUser, error) {
-	return AuthUser{}, nil
+	query := `
+		SELECT id, username, email, password_hash
+		FROM users
+		WHERE username = $1`
+
+	var user AuthUser
+	err := ar.db.QueryRow(query, username).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.PasswordHash,
+	)
+
+	if err != nil {
+		return AuthUser{}, fmt.Errorf("getting user: %w", err)
+	}
+
+	return user, nil
 }
